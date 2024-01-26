bc771fecb35694f23e0aa0e0c32bc07ac4178a93
Afraz Khan <afrazarif@hotmail.com>
2024-01-26 01:35:07 -0800

    Added in some basic JS files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+
+}
+
+function square(number){
+    return number*number;
+}
+
+greet('Afraz');
+console.log(square(2))
